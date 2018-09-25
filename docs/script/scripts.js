eebdea4512aa578e3fdd89a4d6b666310b22a2d6
levysantiago <levyssantiago@gmail.com>
2018-09-24 22:22:34 -0300

    Atualizando sidenav

--- docs/script/scripts.js
+++ docs/script/scripts.js
@@ -10,3 +10,5 @@ document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.parallax');
 	var instances = M.Parallax.init(elems, 0);
 });
+
+screen.lockOrientationUniversal(["landscape-primary", "landscape-secondary"])
