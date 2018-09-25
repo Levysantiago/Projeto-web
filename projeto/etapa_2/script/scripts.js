c00d7427008035216e74f8c24c63e73d50527e36
levysantiago <levyssantiago@gmail.com>
2018-09-24 22:37:27 -0300

    Atualizando animação sidenav

--- projeto/etapa_2/script/scripts.js
+++ projeto/etapa_2/script/scripts.js
@@ -10,5 +10,3 @@ document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.parallax');
 	var instances = M.Parallax.init(elems, 0);
 });
-
-screen.lockOrientationUniversal(["landscape-primary", "landscape-secondary"])
